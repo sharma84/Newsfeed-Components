c880696ccd9e5b68eff93556528eb191b67ca0a5
Snigdha Regmi <sregmi@NORD.LOCAL>
2019-02-05 20:03:42 -0700

    menu.js started

--- Article/Article.js
+++ Article/Article.js
@@ -3,20 +3,18 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+ 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+   
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
   }
+}
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
   }
-}
 
 /* START HERE: 
 
@@ -26,4 +24,3 @@ class Article {
 
 */
 
-let articles;
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,16 @@
 
 const toggleMenu = () => {
+  
   // Toggle the "menu--open" class on your menu refence. 
+  this.classList.toggle('.menu-open');
 }
 
+
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelectorAll('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelectorAll('.menu-button');
+console.log(menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListerner('click', this.toggleMenu);
